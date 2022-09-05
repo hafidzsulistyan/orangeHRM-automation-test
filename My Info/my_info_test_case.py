d9b0b64d0ee7e02a7aa1f88b0f947a1d26d557fc
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2022-09-05 13:16:40 +0700

    Add my info test case

---
+++ My Info/my_info_test_case.py
@@ -0,0 +1,46 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+
+username="Admin"
+password="admin123"
+url="https://opensource-demo.orangehrmlive.com"
+
+class MyInfoTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+    
+    def login(self, driver):
+        driver.find_element(By.NAME, "txtUsername").send_keys(username)
+        time.sleep(0.5)
+        driver.find_element(By.NAME, "txtPassword").send_keys(password)
+        time.sleep(0.5)
+        driver.find_element(By.NAME,"Submit").click()
+        time.sleep(3)
+
+    def test_edit_blood_type(self):
+        driver = self.driver
+        driver.get(url)
+        time.sleep(3)
+
+        self.login(driver)
+
+        driver.find_element(By.ID, "menu_pim_viewMyDetails").click()
+        time.sleep(3)
+
+        driver.find_element(By.ID, "btnEditCustom").click()
+        Select(driver.find_element(By.NAME, "custom1")).select_by_value("B+")
+        time.sleep(0.5)
+        driver.find_element(By.ID, "btnEditCustom").click()
+        time.sleep(3)
+
+        self.assertIn(Select(driver.find_element(By.NAME, "custom1")).first_selected_option.text, "B+")
+
+    def tearDown(self): 
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
